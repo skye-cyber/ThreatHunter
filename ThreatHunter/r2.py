e3378315c978059eaa701d106cde760eefab72b9
Skye <swskye17@gmail.com>
2025-04-19 19:00:34 +0300

    Fix typo
    Signed-off-by: Skye <swskye17@gmail.com>

--- ThreatHunter/r2.py
+++ ThreatHunter/r2.py
@@ -2,7 +2,7 @@ import r2pipe
 import logging
 import logging.handlers
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)-8s %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(levelname)-8s %(message)s")
 logger = logging.getLogger(__name__)
 
 
@@ -13,10 +13,10 @@ def redare2_detection(path: str) -> bool:
             r2.cmd("aaa")  # Analyze all code
             for i in r2.cmdj("afl"):
                 if i["itype"] == "code" and "bad" in i["esil"]:
-                    malware_type = 'Radare2 Detection'
+                    malware_type = "Radare2 Detection"
                     print(f"\x1b[31mMalware detected:\x1b[0m {malware_type}")
                     return True
-        r
+
     except (FileNotFoundError, PermissionError, IsADirectoryError) as e:
         print(f"{e}")
         return False
